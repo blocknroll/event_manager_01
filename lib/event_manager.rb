74d05338c7f8d3563a8693dd870467714149dc27
Fred Block <info@fredblock.com>
2015-01-15 13:24:55 -0700

    Refactored clean_zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,15 +5,17 @@ def clean_zipcode(zipcode)
   # if the zip code is exactly 5 digits, assume that it is ok
   # if the zip code is more than 5 digits, truncate it to the first 5 digits
   # if the zip code is less than 5 digits, add zeros to the front until it becomes 5 digits
-  if zipcode.nil?
-    zipcode = "00000"
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust 5, "0"
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, "0")[0..4]
+
+  # if zipcode.nil?
+  #   zipcode = "00000"
+  # elsif zipcode.length < 5
+  #   zipcode = zipcode.rjust 5, "0"
+  # elsif zipcode.length > 5
+  #   zipcode = zipcode[0..4]
+  # else
+  #   zipcode
+  # end
 end
 
 puts "EventManager Initialized!"
