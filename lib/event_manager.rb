d1dd2ad7235c534237fa802abf032cc952f84893
Fred Block <info@fredblock.com>
2015-01-17 20:21:41 -0700

    Added 'save_thank_you_letters' file output method.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -15,6 +15,16 @@ def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode("80203")
 end
 
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
 
 
 puts "EventManager Initialized!"
@@ -25,12 +35,12 @@ template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
 contents.each do  |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-
   legislators = legislators_by_zipcode(80203)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
+
+  save_thank_you_letters(id,form_letter)
 end
